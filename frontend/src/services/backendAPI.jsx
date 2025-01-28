62cab1bcdc69418d8b47eb9e2def515bee08721f
Halyna Hryn <71042425+GGalina@users.noreply.github.com>
2025-01-28 15:37:47 +0000

    UPDATE: Clean up logs

--- frontend/src/services/backendAPI.jsx
+++ frontend/src/services/backendAPI.jsx
@@ -47,8 +47,6 @@ export const removeListingApi = async (id) => {
 export const getAllListingsApi = async () => {
     try {
         const { data } = await axios.get('/api/');
-        console.log("Fetched data from:", axios.defaults.baseURL, data);
-        console.log("Environment variables:", process.env.REACT_APP_API_BASE_URL);
         return data;
     } catch (error) {
         console.error("Error fetching apartments:", error);
